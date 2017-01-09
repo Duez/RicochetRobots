aad5ecd92ccfefaf81a91c0c9518274d0585db04
duez <marc7duez@gmail.com>
2017-01-09 11:34:01 +0100

    install_libs.sh : small script to download missing libraries

---
+++ install_libs.sh
@@ -0,0 +1,6 @@
+cd ./client
+mkdir compatibility
+cd ./compatibility
+wget https://github.com/processing/p5.js/releases/download/0.5.6/p5.js
+wget http://hammerjs.github.io/dist/hammer.min.js
+wget http://hammerjs.github.io/dist/hammer-time.min.js
